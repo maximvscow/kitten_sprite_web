380130cb326d8ae2b77f1c10a83da4d6b039843a
maximvscow <81038552+maximvscow@users.noreply.github.com>
2024-06-11 22:07:31 +0300

    feed cat
    Создал кнопку по которой можно кормить кота, но возвращение в дефолт не дожидается выполнения цикла с котом

--- docs/game.js
+++ docs/game.js
@@ -12,6 +12,15 @@ var pjs = new PointJS(192, 242, {
   
   //анимационный персонаж в фазе idel
   var idel = game.newAnimationObject({
+    animation: tiles.newImage("sprites/000001/def.png").getAnimation(0, 0, 192, 192, 19),
+    x: spriteMargin,
+    y: 70,
+    w: 192,
+    h: 192,
+    delay: 3,
+  })
+
+  var lick = game.newAnimationObject({
     animation: tiles.newImage("sprites/000001/lick.png").getAnimation(0, 0, 192, 192, 28),
     x: spriteMargin,
     y: 70,
@@ -19,7 +28,19 @@ var pjs = new PointJS(192, 242, {
     h: 192,
     delay: 3,
   })
-  
+
+  var feed = game.newAnimationObject({
+    animation: tiles.newImage("sprites/000001/feed.png").getAnimation(0, 0, 192, 192, 29),
+    x: spriteMargin,
+    y: 70,
+    w: 192,
+    h: 192,
+    delay: 3,
+  })
+
+  var clicked = false
+  document.getElementById('feedCat').addEventListener("click", function() {
+    clicked = true });
   
   // Если количество фаз, задержка, и размер кадров разных фаз персонажа совпадают, можно
   // не создавать два анимационных объекта а менять в параметре animation ссылку на новое изображение
@@ -28,13 +49,18 @@ var pjs = new PointJS(192, 242, {
   
   game.newLoop('myGame', function () { 
 
-    idel.draw(); // иначе рисовать idel
-
     window.addEventListener('resize', function(event) {
       windowInnerWidth = window.innerWidth
       spriteMargin = (windowInnerWidth/2 - 96)
       idel.x = spriteMargin
     }, true);
+
+    if (clicked == true) {
+      feed.draw(); 
+      clicked = false
+    } else {
+      idel.draw(); 
+    }
 
   });
   
