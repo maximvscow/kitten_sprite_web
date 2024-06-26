0a0a20e835c8582d3e7ef66c15b57b0e61f5888d
maximvscow <81038552+maximvscow@users.noreply.github.com>
2024-06-26 21:30:10 +0300

    buttons remake

--- docs/game.js
+++ docs/game.js
@@ -1,16 +1,53 @@
 var windowInnerWidth = window.innerWidth
 var spriteMargin = (windowInnerWidth/2 - 96)
+var overfeed = false
 
 var pjs = new PointJS(192, 242, {
     backgroundColor: '#ffffff'
   });
   
-  pjs.system.initFullPage(); // развернули игру на полный экран
+  pjs.system.initFullPage();
   var game = pjs.game;
   var tiles = pjs.tiles;
-  var key = pjs.keyControl.initControl();
+  var mouse = pjs.mouseControl;
+  mouse.initControl();
   
-  //анимационный персонаж в фазе idel
+  
+  var text_button_feed = game.newTextObject({
+    x: spriteMargin,
+    y: 340,
+    text: "feed",
+    size: 30,
+    font: 'Comicoro',
+    color: "#474646",
+  })
+
+  var text_button_drink = game.newTextObject({
+    x: spriteMargin + 150,
+    y: 340,
+    text: "drink",
+    size: 30,
+    font: 'Comicoro',
+    color: "#474646",
+  })
+
+  var rect_button_feed = game.newRectObject({
+    x: spriteMargin - 30,
+    y: 330,
+    w: 100,
+    h: 50,
+    fillColor: "#FFCCCC",
+  })
+
+  var rect_button_drink = game.newRectObject({
+    x: spriteMargin + 120,
+    y: 330,
+    w: 100,
+    h: 50,
+    fillColor: "#FFCCCC",
+  })
+
+  
   var idel = game.newAnimationObject({
     animation: tiles.newImage("sprites/000001/def.png").getAnimation(0, 0, 192, 192, 19),
     x: spriteMargin,
@@ -38,29 +75,38 @@ var pjs = new PointJS(192, 242, {
     delay: 3,
   })
 
-  var clicked = false
 
   window.addEventListener('resize', function(event) {
-    windowInnerWidth = window.innerWidth
-    spriteMargin = (windowInnerWidth/2 - 96)
-    idel.x = spriteMargin
+    windowInnerWidth = window.innerWidth;
+    spriteMargin = (windowInnerWidth/2 - 96);
+    idel.x = spriteMargin;
+    feed.x = spriteMargin;
+    lick.x = spriteMargin;
+    text_button_feed.x = spriteMargin;
+    rect_button_feed.x = spriteMargin - 30;
+    text_button_drink.x = spriteMargin + 150;
+    rect_button_drink.x = spriteMargin + 120;
   }, true);
 
-  document.getElementById('feedCat').addEventListener("click", function() {
-    clicked = true });
-  
-  // Почему анимация еду отрисовывается только по шестому нажатию на кнопку 
-  
   
   game.newLoop('myGame', function () { 
 
-    if (clicked == true) {
-      feed.draw();
-      if (feed.getFrame() == feed.getLastFrame()){
-        clicked = false
-      }
+    rect_button_feed.draw();
+    rect_button_drink.draw();
+    text_button_feed.draw();
+    text_button_drink.draw();
+    idel.draw(); 
+
+    if (mouse.isInStatic(rect_button_feed.getStaticBox())) {
+      rect_button_feed.fillColor = '#f0b4b4';
     } else {
-      idel.draw(); 
+      rect_button_feed.fillColor = '#FFCCCC';
+    }
+
+    if (mouse.isInStatic(rect_button_feed.getStaticBox())) {
+      rect_button_drink.fillColor = '#f0b4b4';
+    } else {
+      rect_button_drink.fillColor = '#FFCCCC';
     }
 
   });
